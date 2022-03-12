249af41eb06a19039b0a5119cec8f68cc4a0e1b7
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-03-12 07:34:37 +0530

    feat(identity/mod.rs): Add composite key

--- src/identities/mod.rs
+++ src/identities/mod.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use super::features;
+use chrono::{DateTime, Utc};
 
 
 #[derive(Serialize, Deserialize)]
@@ -12,10 +13,36 @@ pub struct Trait{
 pub struct Identity{
     pub identifier: String,
     pub environment_api_key: String,
-    pub created_date: String, // TODO: change to datetime
+    pub created_date: DateTime<Utc>,
     pub identity_features: Vec<features::FeatureState>,
     pub identity_traits: Vec<Trait>,
     pub identity_uuid: String, // TODO: Add default value
-    pub django_id: u32
+    pub django_id: Option<u32>
 
+}
+impl Identity {
+    pub fn composite_key(&self) -> String {
+        return self.environment_api_key.clone() + "_" + &self.identifier
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn composite_key_is_correct(){
+        let expected_composite_key = "test_api_key_test_user";
+        let identity_json = r#"{
+            "identifier": "test_user",
+            "environment_api_key": "test_api_key",
+            "created_date": "2022-03-02T12:31:05.309861+00:00",
+            "identity_features": [],
+            "identity_traits": [],
+            "identity_uuid":""
+        }"#;
+
+        let identity: Identity = serde_json::from_str(identity_json).unwrap();
+        assert_eq!(identity.composite_key(), expected_composite_key)
+    }
 }
