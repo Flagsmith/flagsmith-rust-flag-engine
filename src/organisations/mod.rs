84aff4e3489806ff83d04cdafd9b74d7c947cc4e
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-03-12 06:46:25 +0530

    feat(org/mod): Add unique slug function

--- src/organisations/mod.rs
+++ src/organisations/mod.rs
@@ -8,3 +8,29 @@ pub struct Organisation{
     pub stop_serving_flags: bool,
     pub persist_trait_data: bool
 }
+
+impl Organisation{
+    pub fn unique_slug(&self) -> String {
+        return self.id.to_string() + "_" + &self.name
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn unqiue_slug_is_correct(){
+        let expected_slug = "1_test_org";
+        let organisation_json = r#"{
+            "id": 1,
+            "name": "test_org",
+            "feature_analytics": true,
+            "stop_serving_flags": false,
+            "persist_trait_data": true
+        }"#;
+
+        let organisation: Organisation = serde_json::from_str(organisation_json).unwrap();
+        assert_eq!(organisation.unique_slug(), expected_slug)
+    }
+}
