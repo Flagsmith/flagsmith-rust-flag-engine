0d31481e62ad5bb6e164b07abe1ec2a347268a93
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-03-20 20:27:44 +0530

    fix hash to int conversion

--- src/utils/hashing.rs
+++ src/utils/hashing.rs
@@ -1,4 +1,9 @@
 use md5::{Md5, Digest};
+use num_traits::cast::ToPrimitive;
+
+use num_bigint;
+
+use num_bigint::Sign::{Minus, NoSign, Plus};
 fn to_u32(slice: &[u8]) -> u32 {
     slice.iter().rev().fold(0, |acc, &b| acc*2 + b as u32)
 }
@@ -9,10 +14,10 @@ pub fn get_hashed_percentage_for_object_ids(object_ids: Vec<&str>, iterations: u
         to_hash.push_str(&object_ids.join(","))
     }
     hasher.update(to_hash);
-    // let hashed_value = md5::compute(to_hash);
     let hashed_value = hasher.finalize();
-    let hashed_value_as_int = to_u32(&hashed_value);
-    let value = ((hashed_value_as_int%9999) as f32 /9998.0)*100.0;
+    let hashed_value_as_bigint = num_bigint::BigUint::from_bytes_be(&hashed_value);
+    let hashed_value_as_int = (hashed_value_as_bigint%9999 as u32).to_u32().unwrap();
+    let value = (hashed_value_as_int as f32 /9998.0)*100.0;
     println!("hello hash {:?}", value);
     return value
 }
@@ -22,7 +27,7 @@ mod tests {
 
     #[test]
     fn get_hashed_percentage_for_object_ids_is_the_same_each_time() {
-        get_hashed_percentage_for_object_ids(vec!["boy", "you", "suck"], 1);
+        get_hashed_percentage_for_object_ids(vec!["1", "2"], 1);
     }
 
 
