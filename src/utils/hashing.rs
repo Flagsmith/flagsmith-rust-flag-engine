8ddcc2d9421a746f2529755db9af870136c59804
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2022-03-24 05:56:46 +0530

    refac(hashing/get_hashed_pc)update signature

--- src/utils/hashing.rs
+++ src/utils/hashing.rs
@@ -2,7 +2,7 @@ use md5::{Digest, Md5};
 use num_bigint;
 use num_traits::cast::ToPrimitive;
 
-pub fn get_hashed_percentage_for_object_ids(object_ids: &Vec<&str>, iterations: u32) -> f32 {
+pub fn get_hashed_percentage_for_object_ids(object_ids: Vec<&str>, iterations: u32) -> f32 {
     let mut to_hash = object_ids.join(",");
     let mut hasher = Md5::new();
     for _ in 1..iterations {
@@ -22,21 +22,21 @@ mod tests {
 
     #[test]
     fn hashed_percentage_for_object_ids_is_same_for_same_ids() {
-        fn check(object_ids: &Vec<&str>) {
-            let first_hashed_percetnage = get_hashed_percentage_for_object_ids(&object_ids, 1);
-            let second_hashed_percetnage = get_hashed_percentage_for_object_ids(&object_ids, 1);
+        fn check(object_ids: Vec<&str>) {
+            let first_hashed_percetnage = get_hashed_percentage_for_object_ids(object_ids.clone(), 1);
+            let second_hashed_percetnage = get_hashed_percentage_for_object_ids(object_ids, 1);
             assert_eq!(first_hashed_percetnage, second_hashed_percetnage);
         }
-        check(&vec!["1", "2"]);
-        check(&vec![&Uuid::new_v4().to_hyphenated().to_string(), "2"])
+        check(vec!["1", "2"]);
+        check(vec![&Uuid::new_v4().to_hyphenated().to_string(), "2"])
     }
 
     #[test]
     fn hashed_percentage_for_object_ids_is_different_for_different_ids() {
         let first_object_ids = vec!["1", "2"];
         let second_object_ids = vec!["9", "10"];
-        let first_hashed_percetnage = get_hashed_percentage_for_object_ids(&first_object_ids, 1);
-        let second_hashed_percetnage = get_hashed_percentage_for_object_ids(&second_object_ids, 1);
+        let first_hashed_percetnage = get_hashed_percentage_for_object_ids(first_object_ids, 1);
+        let second_hashed_percetnage = get_hashed_percentage_for_object_ids(second_object_ids, 1);
         assert!(first_hashed_percetnage != second_hashed_percetnage);
     }
 }
